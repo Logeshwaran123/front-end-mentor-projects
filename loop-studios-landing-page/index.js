aaccbc662a4b984baea52e38a8f42d4cf17218cb
Logeshwaran123 <hlhilalogu@gmail.com>
2024-05-21 21:31:11 +0530

    Initiated Responsive Design

--- loop-studios-landing-page/index.js
+++ loop-studios-landing-page/index.js
@@ -1,34 +1,12 @@
 "use strict";
 
-// DOM Elements
-const hamMenu = document.querySelector(".ham-menu");
-const closeMenu = document.querySelector(".close-menu");
-const navPanel = document.querySelector(".nav-panel");
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-const headerBack = document.querySelector(".black-background");
-const heroContent = document.querySelector(".hero-content");
+// DOM elements
+const menuIcons = document.querySelectorAll(".ico");
 
-function changeIconState() {
-  this.classList.remove("active-menu");
-  if (this.alt === "ham-menu") {
-    navPanel.classList.add("active-nav-panel");
-    closeMenu.classList.add("active-menu");
-    // Remove Display Contents
-    main.style.display = "none";
-    headerBack.style.opacity = 1;
-    heroContent.style.zIndex = -1;
-    heroContent.style.opacity = 0;
-  } else {
-    hamMenu.classList.add("active-menu");
-    navPanel.classList.remove("active-nav-panel");
-    headerBack.style.opacity = 0;
-    heroContent.style.zIndex = 0;
-    heroContent.style.opacity = 1;
-    main.style.display = "block";
-  }
-}
-
-// NAV PANEL
-hamMenu.addEventListener("click", changeIconState);
-closeMenu.addEventListener("click", changeIconState);
+menuIcons.forEach((cur, ind) => {
+  cur.addEventListener("click", function () {
+    let index = cur.classList.contains("ico-ham") ? 1 : 0;
+    cur.classList.remove("active-icon");
+    menuIcons[index].classList.add("active-icon");
+  });
+});
